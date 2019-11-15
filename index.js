8d57f42d6fe6db99c23ebcd0fe397b2e20f383a9
Kristin Kraemer <kristinhannahk@gmail.com>
2019-11-15 00:31:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 
 const removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
+
+prepend.unshift("Odie")
+
+removeLast.pop()
+
+removeFirst.shift()
